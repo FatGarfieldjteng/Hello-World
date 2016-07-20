7f13c2828ce8996ffef196623b6b0787105107e2
FatGarfieldjteng <jun.teng@autodesk.com>
2016-07-20 17:55:12 +0800

    Add main

---
+++ print/print.cpp
@@ -0,0 +1,13 @@
+// print.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is a test!" << std::endl;
+
+    return 0;
+}
+
